9c33406ea2054511c9538b179ffbf94bf42b8a83
istrelkina <strelaweb@gmail.com>
2020-03-25 10:05:37 +1000

    added catalog

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,15 @@ $(function(){
 		autoplay: true,
 		autoplaySpeed: 5000
 	});
+
+	$('.catalog__item').mouseenter(
+		function(){
+			$(this).children('.catalog__item-info').css('background-color', '#fff');
+			$(this).children('.catalog__item-info').children('.item-info-price').css('background-color', '#f7f7f7');
+		})
+	$('.catalog__item').mouseleave(
+		function(){
+			$(this).children('.catalog__item-info').css('background-color', '#f7f7f7');
+			$(this).children('.catalog__item-info').children('.item-info-price').css('background-color', '#fff');
+		})
 });
